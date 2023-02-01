58781f00e33f5cfe10411647e01f882f641339d5
Montywilliams <3650@holbertonschool.com>
2023-02-01 00:01:39 -0600

    correct typo

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -44,7 +44,7 @@ function CreatePost() {
         if(form.prompt && form.photo) {
             setLoading(true);
             try {
-                const response = await fetch('https://dalle-ai-gen.onrender.com//api/v1/post', {
+                const response = await fetch('https://dalle-ai-gen.onrender.com/api/v1/post', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
